e7a1afe54d47bccec886d6a67f516fb9b98a9687
luigi-borriello00 <luigi-borriello00@users.noreply.github.com>
2026-03-30 23:07:31 +0000

    deploy: 687d459b01e71239f24af081802939ed39a9eded

--- js/parser.js
+++
@@ -1,9 +0,0 @@
-const DATA_URL = 'data/questions.json';
-
-export async function loadQuestions() {
-  const response = await fetch(DATA_URL);
-  if (!response.ok) {
-    throw new Error(`Failed to load questions: ${response.status}`);
-  }
-  return response.json();
-}
